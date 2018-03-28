189c97113b870d7d24e027f77da9b62ffc967550
Nico Mollet <nico.mollet@gmail.com>
2018-03-28 10:53:08 +0200

    Update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ### Unreleased ###
 
+### 1.0.1: March 28th, 2018
+* Remove sales at execution of cron
 
-### 1.0.0: MArch 27th, 2018
+### 1.0.0: March 27th, 2018
 * Creation of this project
--- tmsm-woocommerce-onsale-badge.php
+++ tmsm-woocommerce-onsale-badge.php
@@ -31,7 +31,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'TMSM_WOOCOMMERCE_ONSALE_BADGE_VERSION', '1.0.0' );
+define( 'TMSM_WOOCOMMERCE_ONSALE_BADGE_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
